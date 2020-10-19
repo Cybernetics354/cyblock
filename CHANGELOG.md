e1989aeff40e5aabe6c3ad1d742ca344b008a6e8
Cybernetics Core <corecybernetics354@gmail.com>
2020-10-20 01:37:56 +0700

    Dart score

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,9 @@
 ## [0.0.1+1] - 2020-10-10
 
 * Updating README.md
+
+## [0.0.1+2] - 2020-10-10
+
+* Longer description
+* Format core with dartfmt
+* Update dependencies
--- lib/services/core.dart
+++ lib/services/core.dart
@@ -8,42 +8,42 @@ abstract class Cyblock<T, S> {
     state = initState;
     _inStream.add(initState);
   }
-  
+
   final _streamController = new StreamController<T>.broadcast();
   StreamSink<T> get _inStream => _streamController.sink;
 
   /// If you want to stream on the cybloc you can use [stream]
-  /// 
+  ///
   /// [SomeCyblocClass.stream]
   Stream<T> get stream => _streamController.stream;
-  
+
   final _eventController = new StreamController<S>.broadcast();
   StreamSink<S> get _eventListen => _eventController.sink;
-  
+
   /// Use [emit] to update state
   void emit(T data) {
-     state = data;
+    state = data;
     _inStream.add(data);
   }
-  
+
   /// Disposing Stream Controller
   void dispose() {
     _streamController?.close();
     _eventController?.close();
   }
-  
+
   /// Use [insertEvent] to insert event
   void insertEvent(S event) {
     _eventListen.add(event);
   }
-  
+
   /// mapping event to state
   void mapEventToState(S event);
 
   getState() {
     _inStream.add(state);
-  }  
-  
+  }
+
   Cyblock() {
     _eventController.stream.listen(mapEventToState);
   }
